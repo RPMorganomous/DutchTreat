56d8e0f18715154761401c6b5de28c8a28e69454
Richard Morgan <mba.rick@gmail.com>
2018-01-07 14:29:11 -0600

    added jquery

--- DutchTreat/DutchTreat/wwwroot/js/index.js
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -1,1 +1,14 @@
-﻿
+﻿console.log("Hello!");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+	console.log("Buying Item");
+});
+
+var productInfo = document.getElementByClassName("product-props");
+var listItems = productInfo.item[0].children;
+
+
